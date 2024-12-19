b66de1dee06b7e974f20aadd400c776a22ad16f5
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-12-18 23:21:33 -0300

    feat: dnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { FaBox, FaFistRaised, FaShieldAlt } from 'react-icons/fa';
-import './App.css'
-import Modal from 'react-modal';
-import { useState } from 'react';
-import './card.css'
-import { GiSwordBrandish, GiTrapMask } from 'react-icons/gi';
-import retiredPalading from './assets/cards/retiredPaladin.webp'
-import blueDragon from './assets/cards/blueDragon.webp'
-import cardBack from './assets/cards/cardBack.webp'
-import { BiHeart } from 'react-icons/bi';
-import Evy from './assets/cards/evy.webp'
-import Yona from './assets/cards/yona.webp'
-import Mimic from './assets/cards/mimic.webp'
-import ShieldMan from './assets/cards/shieldMan.webp'
-import Hydra from './assets/cards/hydra.webp'
-import { DndContext, DragEndEvent, useDraggable, useDroppable } from '@dnd-kit/core';
+import { FaBox, FaFistRaised, FaShieldAlt } from "react-icons/fa";
+import "./App.css";
+import Modal from "react-modal";
+import { useState } from "react";
+import "./card.css";
+import { GiSwordBrandish, GiTrapMask } from "react-icons/gi";
+import retiredPalading from "./assets/cards/retiredPaladin.webp";
+import blueDragon from "./assets/cards/blueDragon.webp";
+import cardBack from "./assets/cards/cardBack.webp";
+import { BiHeart } from "react-icons/bi";
+import Evy from "./assets/cards/evy.webp";
+import Yona from "./assets/cards/yona.webp";
+import Mimic from "./assets/cards/mimic.webp";
+import ShieldMan from "./assets/cards/shieldMan.webp";
+import Hydra from "./assets/cards/hydra.webp";
+import {
+  DndContext,
+  DragEndEvent,
+  useDraggable,
+  useDroppable,
+} from "@dnd-kit/core";
 
-
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 interface ICard {
   id: number;
@@ -25,6 +29,7 @@ interface ICard {
   attack: number;
   defense: number;
   description: string;
+  type: "monster" | "trap" | "spell";
 }
 
 interface IZone {
@@ -34,9 +39,32 @@ interface IZone {
 
 interface ICardProps {
   card: ICard;
+  openModal: (card: ICard) => void;
 }
 
-function Card({ card }: ICardProps) {
+function Card({ card, openModal }: ICardProps) {
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragTimeout, setDragTimeout] = useState<number | undefined>();
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    const timeout = setTimeout(() => {
+      setIsDragging(true);
+    }, 200);
+    setDragTimeout(timeout);
+  };
+
+  const handleMouseUp = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    if (!isDragging) {
+      openModal(card);
+    }
+    setIsDragging(false);
+    clearTimeout(dragTimeout);
+  };
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: card.id,
   });
@@ -54,6 +82,9 @@ function Card({ card }: ICardProps) {
       className="card-thumbnail"
       {...listeners}
       {...attributes}
+      onMouseDown={(e) => handleMouseDown(e)}
+      onMouseUp={(e) => handleMouseUp(e)}
+      onMouseLeave={() => clearTimeout(dragTimeout)}
     >
       <img src={card.image} alt={`Carta ${card.id}`} />
     </div>
@@ -67,8 +98,8 @@ function Zone({ id, children }: IZone) {
 
   const style = {
     backgroundColor: isOver ? "lightblue" : "transparent",
-    padding: "1rem",
-    border: "1px dashed #ccc",
+    border: "none",
+    width: "100%",
   };
 
   return (
@@ -77,7 +108,6 @@ function Zone({ id, children }: IZone) {
     </div>
   );
 }
-
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -93,23 +123,15 @@ function App() {
     setSelectedCard(null);
   };
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (over) {
-      console.log(`Card ${active.id} dropped in ${over.id}`);
-      // Adicione lógica para atualizar o estado ou mover a carta
-    }
-  };
-
-  const cards = [
+  const cards: ICard[] = [
     {
       id: 1,
       elixir: 1,
       image: retiredPalading,
       attack: 2,
       defense: 2,
-      description: 'Um guerreiro cansado que já viu muitas batalhas.',
+      description: "Um guerreiro cansado que já viu muitas batalhas.",
+      type: "monster",
     },
     {
       id: 2,
@@ -117,7 +139,8 @@ function App() {
       image: blueDragon,
       attack: 8,
       defense: 9,
-      description: 'Lendário dragão azul que cospe fogo.',
+      description: "Lendário dragão azul que cospe fogo.",
+      type: "monster",
     },
     {
       id: 3,
@@ -125,7 +148,9 @@ function App() {
       image: Evy,
       attack: 3,
       defense: 5,
-      description: 'Uma feiticeira misteriosa que cura um aliado em 4 de vida no início de cada turno seu.',
+      description:
+        "Uma feiticeira misteriosa que cura um aliado em 4 de vida no início de cada turno seu.",
+      type: "monster",
     },
     {
       id: 4,
@@ -133,7 +158,8 @@ function App() {
       image: Yona,
       attack: 8,
       defense: 5,
-      description: 'Uma guerreira que ataca com uma espada mágica.',
+      description: "Uma guerreira que ataca com uma espada mágica.",
+      type: "monster",
     },
     {
       id: 5,
@@ -141,7 +167,8 @@ function App() {
       image: Mimic,
       attack: 2,
       defense: 2,
-      description: 'Copia as últimas duas cartas que seu oponente jogou.',
+      description: "Copia as últimas duas cartas que seu oponente jogou.",
+      type: "monster",
     },
     {
       id: 6,
@@ -149,7 +176,8 @@ function App() {
       image: ShieldMan,
       attack: 1,
       defense: 6,
-      description: 'Um guerreiro que defende com um escudo.',
+      description: "Um guerreiro que defende com um escudo.",
+      type: "monster",
     },
     {
       id: 7,
@@ -157,125 +185,185 @@ function App() {
       image: Hydra,
       attack: 9,
       defense: 9,
-      description: 'Destrua um inimigo com menos ataque.',
+      description: "Destrua um inimigo com menos ataque.",
+      type: "monster",
     },
   ];
+  const cartsInEnemyHand = [
+    {
+      id: 6,
+      elixir: 2,
+      image: ShieldMan,
+      attack: 1,
+      defense: 6,
+      description: "Um guerreiro que defende com um escudo.",
+      type: "monster",
+    },
+  ];
+  const [cardsInHand, setCardsInHand] = useState<ICard[]>(cards || []);
+  const [cardsInAttackZone, setCardsInAttackZone] = useState<ICard[]>([]);
+  const [cardsInDefenseZone, setCardsInDefenseZone] = useState<ICard[]>([]);
+  const [cardsInTrapZone, setCardsInTrapZone] = useState<ICard[]>([]);
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (over) {
+      const activeCard = cardsInHand.find((card) => card.id === active.id);
+      if (activeCard) {
+        if (over.id === "trap" && activeCard.type !== "trap") {
+          return;
+        }
+        if (over.id === "attack") {
+          setCardsInAttackZone([...cardsInAttackZone, activeCard]);
+        } else if (over.id === "defense") {
+          setCardsInDefenseZone([...cardsInDefenseZone, activeCard]);
+        } else if (over.id === "trap") {
+          setCardsInTrapZone([...cardsInTrapZone, activeCard]);
+        }
+        setCardsInHand(cardsInHand.filter((card) => card.id !== active.id));
+      }
+    }
+  };
+
   return (
     <DndContext onDragEnd={(e) => handleDragEnd(e)}>
-    <div className="game-board">
-     <div className="opponent-area">
-     <div className="zone hand-zone">
-      <div className="hand-header">
-        <FaFistRaised className="icon" />
-        <div className="life-points">
-          <BiHeart className="life-icon" />
-          <span>30</span>
-        </div>
-        <div className="deck">
-          <FaBox className="deck-icon" />
-          <span>18</span>
-        </div>
-      </div>
-      <div className="card-container">
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className="card-thumbnail"
-          >
-            <img src={cardBack} alt={`Fundo de carta`} />
-          </div>
-        ))}
-      </div>
-    </div>
-    <div className="zone trap-zone">
-      <GiTrapMask className="icon" />
-      <span>Armadilhas</span>
-    </div>
-    <div className="zone attack-zone">
-      <GiSwordBrandish className="icon" />
-      <span>Ataque</span>
-    </div>
-
-    <div className="zone defense-zone">
-      <FaShieldAlt className="icon" />
-      <span>Defesa</span>
-    </div>
-
-  </div>
-  <div className="divider">VS</div>
-
-  <div className="player-area">
-    <div className="zone defense-zone">
-    <Zone id="defense">
-      <FaShieldAlt className="icon" />
-      <span>Defesa</span>
-    </Zone>
-    </div>
-
-    <div className="zone attack-zone">
-    <Zone id="attack">
-      <GiSwordBrandish className="icon" />
-      <span>Ataque</span>
-    </Zone>
-    </div>
-
-    <div className="zone trap-zone">
-    <Zone id="trap">
-      <GiTrapMask className="icon" />
-      <span>Armadilhas</span>
-    </Zone>
-    </div>
-
-    <div className="zone hand-zone">
-      <div className="hand-header">
-        <FaFistRaised className="icon" />
-        <div className="life-points">
-          <BiHeart className="life-icon" />
-          <span>30</span>
-        </div>
-        <div className="deck">
-          <FaBox className="deck-icon" />
-          <span>18</span>
-        </div>
-      </div>
-      <div className="card-container">
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className="card-thumbnail"
-            onClick={() => openModal(card)}
-          >
-           <Card card={card} />
-          </div>
-        ))}
-      </div>
-    </div>
-  </div>
-
-      {/* Modal */}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Detalhes da Carta"
-        className="modal"
-        overlayClassName="overlay"
-      >
-        {selectedCard && (
-          <div className="card-details">
-            <div className="modal-header">Elixir: {selectedCard.elixir}</div>
-            <img src={selectedCard.image} alt="Carta" />
-            <div className="modal-footer">
-              <div>Ataque: {selectedCard.attack}</div>
-              <div>Defesa: {selectedCard.defense}</div>
+      <div className="game-board">
+        <div className="opponent-area">
+          <div className="zone hand-zone">
+            <div className="hand-header">
+              <FaFistRaised className="icon" />
+              <div className="life-points">
+                <BiHeart className="life-icon" />
+                <span>30</span>
+              </div>
+              <div className="deck">
+                <FaBox className="deck-icon" />
+                <span>18</span>
+              </div>
             </div>
-            <div className="modal-description">{selectedCard.description}</div>
-            <button onClick={closeModal}>Fechar</button>
+            <div className="card-container">
+              {cartsInEnemyHand.map((card) => (
+                <div key={card.id} className="card-thumbnail">
+                  <img src={cardBack} alt={`Fundo de carta`} />
+                </div>
+              ))}
+            </div>
           </div>
-        )}
-      </Modal>
-    </div>
+          <div className="zone trap-zone">
+            <GiTrapMask className="icon" />
+          </div>
+          <div className="zone attack-zone">
+            <GiSwordBrandish className="icon" />
+          </div>
+
+          <div className="zone defense-zone">
+            <FaShieldAlt className="icon" />
+          </div>
+        </div>
+        <div className="divider">VS</div>
+
+        <div className="player-area">
+          <div className="zone defense-zone">
+            <Zone id="defense">
+              <FaShieldAlt className="icon" />
+              <div className="card-container">
+              {cardsInDefenseZone.map((card) => (
+                <div
+                  key={card.id}
+                  onClick={() => openModal(card)}
+                  className="card-thumbnail"
+                >
+                  <img src={card.image} alt={`Carta ${card.id}`} />
+                </div>
+              ))}
+              </div>
+            </Zone>
+          </div>
+
+          <div className="zone attack-zone">
+            <Zone id="attack">
+              <GiSwordBrandish className="icon" />
+              <div className="card-container">
+                  {cardsInAttackZone.map((card) => (
+                    <div
+                      key={card.id}
+                      onClick={() => openModal(card)}
+                      className="card-thumbnail"
+                    >
+                      <img src={card.image} alt={`Carta ${card.id}`} />
+                    </div>
+                  ))}
+              </div>
+            </Zone>
+          </div>
+
+          <div className="zone trap-zone">
+            <Zone id="trap">
+              <GiTrapMask className="icon" />
+              {
+                <div className="card-container">
+                  {cardsInTrapZone.map((card) => (
+                    <div
+                      key={card.id}
+                      onClick={() => openModal(card)}
+                      className="card-thumbnail"
+                    >
+                      <img src={card.image} alt={`Carta ${card.id}`} />
+                    </div>
+                  ))}
+                </div>
+              }
+            </Zone>
+          </div>
+
+          <div className="zone hand-zone">
+            <div className="hand-header">
+              <FaFistRaised className="icon" />
+              <div className="life-points">
+                <BiHeart className="life-icon" />
+                <span>30</span>
+              </div>
+              <div className="deck">
+                <FaBox className="deck-icon" />
+                <span>18</span>
+              </div>
+            </div>
+            <div className="card-container">
+              {cardsInHand.map((card) => (
+                <div key={card.id} className="card-thumbnail">
+                  <Card openModal={(card) => openModal(card)} card={card} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* Modal */}
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Detalhes da Carta"
+          className="modal"
+          overlayClassName="overlay"
+        >
+          {selectedCard && (
+            <div className="card-details">
+              <div className="modal-header">Elixir: {selectedCard.elixir}</div>
+              <img src={selectedCard.image} alt="Carta" />
+              <div className="modal-footer">
+                <div>Ataque: {selectedCard.attack}</div>
+                <div>Defesa: {selectedCard.defense}</div>
+              </div>
+              <div className="modal-description">
+                {selectedCard.description}
+              </div>
+              <button onClick={closeModal}>Fechar</button>
+            </div>
+          )}
+        </Modal>
+      </div>
     </DndContext>
   );
 }
 
-export default App
+export default App;
